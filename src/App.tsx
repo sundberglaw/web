b32a6868327c132c8c774efc2ace2eeb080674da
Sean Sundberg <seansund@us.ibm.com>
2022-10-18 12:21:23 -0500

    Builds initial version of site
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Grid} from '@mui/material'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="top-menu">
+        <div><img src='images/kelly-logo-right.png' alt='logo'></img></div>
+        </div>
+        <Grid container spacing={2} paddingLeft='40px' paddingRight='40px' paddingTop={'40px'}>
+          <Grid item xs={6} md={4}>
+            <img width='100%' src='images/kelly-headshot-gray.jpg' alt='headshot'></img>
+          </Grid>
+          <Grid item xs={6} md={8} lg={4}>
+            <div style={{textAlign: 'left'}}>
+              <p>
+                Kelly Sundberg has been providing clients in Williamson and surrounding counties with trusted
+                legal services for over 20 years. She works in areas of guardianship, probate, 
+                wills, trusts and estates, and family law.
+              </p>
+              <p>
+                Call today to make an appointment - <a style={{color: 'white'}} href="tel:512-246-3995">512-246-3995</a>.
+                The offices of Kelly Sundberg, PC are located in downtown Round Rock. Virtual and in-person meetings are
+                available.
+              </p>
+            </div>
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} className='footer' paddingLeft='140px' paddingRight='140px'>
+          <Grid item xs={12} md={4}>
+            407 W Liberty Ave Round Rock, TX 78664
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <a style={{color: 'white'}} href="tel:512-246-3995">512-246-3995</a>
+          </Grid>
+          <Grid item xs={12} md={4}>
+            <a style={{color: 'white'}} href="mailto:kelly@kellysundberglaw.com">kelly@kellysundberglaw.com</a>
+          </Grid>
+        </Grid>
+
       </header>
     </div>
   );
